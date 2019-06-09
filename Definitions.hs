6bdf73d13818604da51944fa2271febd5e5b6b98
ziurfreelance <ziurfreelance@gmail.com>
2019-06-09 19:03:26 -0300

    las primeras funciones definidas
    Se crearon varias funciones auxiliares generalas para poder extraer los diferentes conjuntos de la redSocial,
    
    Las funciones nombresDeUsuarios, amigosDe, y cantidadDeAmigos ya estan

---
+++ Definitions.hs
@@ -0,0 +1,31 @@
+module Definitions
+where
+
+type Set a = [a]
+type Usuario = (Integer, String) -- (id, nombre)
+type Relacion = (Usuario, Usuario) -- usuarios que se relacionan
+type Publicacion = (Usuario, String, Set Usuario) -- (usuario que publica, texto publicacion, likes)
+type RedSocial = (Set Usuario, Set Relacion, Set Publicacion)
+
+-- Funciones basicas
+
+usuarios :: RedSocial -> Set Usuario
+usuarios (us, _, _) = us
+
+relaciones :: RedSocial -> Set Relacion
+relaciones (_, rs, _) = rs
+
+publicaciones :: RedSocial -> Set Publicacion
+publicaciones (_, _, ps) = ps
+
+idDeUsuario :: Usuario -> Integer
+idDeUsuario (id, _) = id 
+
+nombreDeUsuario :: Usuario -> String
+nombreDeUsuario (_, nombre) = nombre 
+
+usuarioDePublicacion :: Publicacion -> Usuario
+usuarioDePublicacion (u, _, _) = u
+
+likesDePublicacion :: Publicacion -> Set Usuario
+likesDePublicacion (_, _, us) = us
---
+++ Jeferson.hs
@@ -0,0 +1,69 @@
+import Definitions
+
+usuariosPrueba = [(001, "Jeferson"),(002, "Juan"),(003, "Karen"),(004, "Camila"),(005, "Jhonatan")]
+publicacionesPrueba = [((001,"Jeferson"), "Publicacion de prueba de Jeferson", [(004, "Camila"),(006, "Jhonatan")]),((003, "Karen"), "Me llamo Karen", [(002, "Juan")]),((006, "Jhonatan"), "Otra prueba Jhonatan",[])] 
+relacionesPrueba = [((001,"Jeferson"),(003, "Karen")),((002, "Juan"),(003, "Karen")),((004, "Camila"),(006, "Jhonatan")),((001, "Jeferson"),(006, "Jhonatan"))]
+redSocialPrueba = ([(001, "Jeferson"),(002, "Juan"),(003, "Karen"),(004, "Camila"),(006, "Jhonatan")], [((001,"Jeferson"),(003, "Karen")),((002, "Juan"),(003, "Karen")),((004, "Camila"),(006, "Jhonatan")),((001, "Jeferson"),(006, "Jhonatan"))], [((001,"Jeferson"), "Publicacion de prueba de Jeferson", [(004, "Camila"),(006, "Jhonatan")]),((003, "Karen"), "Me llamo Karen", [(002, "Juan")]),((006, "Jhonatan"), "Otra prueba Jhonatan",[])] )
+
+-- FUnciones auxiliares más generales
+usuariosDeLaRed :: RedSocial -> Set Usuario
+usuariosDeLaRed (users, _, _) = users
+
+relacionesDeLaRed :: RedSocial -> Set Relacion
+relacionesDeLaRed (_, relaciones, _) = relaciones
+
+publicacionesDeLaRed :: RedSocial -> Set Publicacion
+publicacionesDeLaRed (_, _, publicaciones) = publicaciones
+
+-- Dada una red social retorna un conjunto con los nombres de todos los usuarios.
+nombresDeUsuarios :: RedSocial -> Set String
+nombresDeUsuarios red = obtenerNombresDeUsuarios (usuariosDeLaRed red)
+
+obtenerNombresDeUsuarios :: Set Usuario -> Set String
+obtenerNombresDeUsuarios [] = []
+obtenerNombresDeUsuarios ( user:users ) = ( snd user ) : obtenerNombresDeUsuarios users
+
+
+-- Dada una red social y un usuario retorna el conjunto de amigos del mismo
+amigosDe :: RedSocial -> Usuario -> Set Usuario
+amigosDe red usuario = amigosDeAux listaRelaciones listaUsuarios usuario
+                     where listaUsuarios = usuariosDeLaRed red
+                           listaRelaciones = relacionesDeLaRed red
+                                 
+amigosDeAux :: Set Relacion -> Set Usuario -> Usuario -> Set Usuario
+amigosDeAux [] _ _  = []
+amigosDeAux _ [] _ = [] 
+amigosDeAux relaciones usuarios usuario | (elem (usuario, head usuarios) relaciones) || (elem (head usuarios, usuario) relaciones) = (head usuarios) : (amigosDeAux relaciones (tail usuarios) usuario)
+                                        | otherwise = (amigosDeAux relaciones (tail usuarios) usuario)
+
+-- Dada una red social y un usuario retorna la cantidad de amigos de dicho usuario
+cantidadDeAmigos :: RedSocial -> Usuario -> Int
+cantidadDeAmigos red usuario = length (amigosDe red usuario)
+
+-- Dada una red social retorna el usuario con mas amigos. De existir más de uno devuelve cualquiera de ellos.
+usuarioConMasAmigos :: RedSocial -> Usuario
+usuarioConMasAmigos = undefined
+
+-- Dada una red social retorna True si algún usuario tiene más de un millón de amigos
+estaRobertoCarlos :: RedSocial -> Bool
+estaRobertoCarlos = undefined
+
+-- Dada una red social y un usuario retorna el conjunto de publicaciones del mismo.
+publicacionesDe :: RedSocial -> Usuario -> Set Publicacion
+publicacionesDe = undefined
+
+-- Dada una red social y un usuario retorna el conjunto de publicaciones a las que el usuario les dió like.
+publicacionesQueLeGustanA :: RedSocial -> Usuario -> Set Publicacion
+publicacionesQueLeGustanA = undefined
+
+-- Dada una red social y dos usuarios indica si les gustan las mismas publicaciones
+lesGustanLasMismasPublicaciones :: RedSocial -> Usuario -> Usuario -> Bool
+lesGustanLasMismasPublicaciones = undefined
+
+-- Dada una red social y un usuario u, indica si existe un usuario que le puso like a todas las publicaciones de u.
+tieneUnSeguidorFiel :: RedSocial -> Usuario -> Bool
+tieneUnSeguidorFiel = undefined
+
+-- Dada una red social y dos usuarios, indica si existe una secuencia de usuarios relacionados para llegar del primero al segundo.
+existeSecuenciaDeAmigos :: RedSocial -> Usuario -> Usuario -> Bool
+existeSecuenciaDeAmigos = undefined
